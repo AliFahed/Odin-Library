76dad3706e53bd7bcbb864d799929926f6d13d5e
Ali Al-Dhib <alifahed67@gmail.com>
2022-07-31 08:09:59 +0800

    Add create book elements function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
+const gridContainer = document.querySelector('[data-grid-container]');
 const bookContainer = document.querySelector('[data-book-container]');
 const bookTitle = document.querySelector('[data-book-title]');
 const bookAuthor = document.querySelector('[data-book-author]');
 const bookPages = document.querySelector('[data-book-pages]');
 const bookStatusBtn = document.querySelector('[data-book-status]');
 const bookRemoveBtn = document.querySelector('[data-book-remove]');
-
 
 let library = [];  // all the books will be stored in this array
 
@@ -24,3 +24,30 @@ function addBookToLibrary(title, author, pages, bookStatus) {
   // console.log(library);
 }
 
+function createBookElements() {
+  const book_container = document.createElement('div');
+  book_container.classList.add('book-container');
+  gridContainer.appendChild(book_container);
+
+  const book_title = document.createElement('div');
+  book_title.classList.add('book-title');
+  book_container.appendChild(book_title);
+
+  const book_author = document.createElement('div');
+  book_author.classList.add('book-author');
+  book_container.appendChild(book_author);
+
+  const book_pages = document.createElement('div');
+  book_pages.classList.add('book-pages');
+  book_container.appendChild(book_pages);
+
+  const statusButton = document.createElement('button');
+  statusButton.classList.add('book-status-btn');
+  book_container.appendChild(statusButton);
+
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('book-remove-btn');
+  book_container.appendChild(removeButton);
+}
+
+createBookElements();
